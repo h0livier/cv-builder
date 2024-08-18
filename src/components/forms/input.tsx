984ebee27acf81d5356a3130f231f25aa8e5927c
Hayot Olivier <olivier.hayot.dev@gmail.com>
2024-08-18 17:12:20 +0200

    finished profile

--- src/components/forms/input.tsx
+++ src/components/forms/input.tsx
@@ -1,4 +1,4 @@
-import {FormControl, FormHelperText, FormLabel, Input} from "@chakra-ui/react";
+import {FormControl, FormHelperText, FormLabel, Input, Textarea} from "@chakra-ui/react";
 
 export interface FormInputProps{
     name: string,
@@ -8,9 +8,20 @@ export interface FormInputProps{
     placeholder?: string,
     value?:string,
     updateValue?: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    updateTextAreaValue?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
 }
 
-export default function FormInput({name, label, helpText, placeholder, value, updateValue, type = 'text'}: FormInputProps): JSX.Element {
+export default function FormInput({name, label, helpText, placeholder, value, updateValue, updateTextAreaValue, type = 'text'}: FormInputProps): JSX.Element {
+
+    if(type === 'textarea'){
+        return (
+            <FormControl size='md'>
+                <FormLabel>{label}</FormLabel>
+                <Textarea name={name} placeholder={placeholder} value={value} onChange={updateTextAreaValue} />
+                <FormHelperText>{helpText}</FormHelperText>
+            </FormControl>
+        )
+    }
 
     return (
         <FormControl size='md'>
--- src/components/sections/profile.tsx
+++ src/components/sections/profile.tsx
@@ -1,15 +1,38 @@
-import {FormControl, FormLabel, Stack, Textarea} from "@chakra-ui/react";
+import {Button, FormControl, FormLabel, Stack, Textarea} from "@chakra-ui/react";
+import FormInput from "../forms/input";
+import { useEffect, useState } from "react";
+import { useStorage } from "@/hooks/useStorage";
 
 
 export default function ProfileSection(){
 
+    const [description, setDescription] = useState({
+        profile: ''
+    })
+
+    const [profile, setProfile] = useStorage('local', 'profile', []);
+
+    useEffect(() => {
+        if(profile){
+            setDescription({profile: profile})
+        }
+    }, [])
+
+    function handleInputChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        const { name, value } = event.target;
+        setDescription(prevFormData => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+    }
+
     return(
         <>
-            <Stack spacing={3} direction='row'>
-                <FormControl>
-                    <FormLabel>Description</FormLabel>
-                    <Textarea />
-                </FormControl>
+            <Stack spacing={3}>
+                <FormInput label="" name='profile' type="textarea" value={description.profile} updateTextAreaValue={handleInputChange} />
+                <Stack direction='row'>
+                    <Button flexGrow={1} onClick={() => setProfile(description.profile)}>Enregistrer les modifications</Button>
+                </Stack>
             </Stack>
         </>
     )
